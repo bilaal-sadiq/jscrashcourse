b847844792f6e1616f75f559e7e211804c3fe07c
mrmbs1 <mbilalsadiq.19@gmail.com>
2023-09-10 15:49:00 +0100

    Conditonals introduction

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-/*
-* Converting Temperatures 
-*
-* Celsius-to-Fahrenheit formula 
-*
-* F = C x 1.8 + 32 
+let subscribed = false
+let loggedIn = true
 
-
-let celsius = 10
-let fahrenheit = celsius * 1.8 + 32
-
-console.log(fahrenheit) */
-
+if (subscribed === true) {
+    console.log('Show the video')
+}
+else if (loggedIn === true) {
+    console.log('tell the user to upgrade their subscription')
+}
+else {
+    console.log ('tell user to log into account')
+}
