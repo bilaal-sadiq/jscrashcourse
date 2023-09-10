257b270a68b99c2c91946817ae4238e51d97cc6f
mrmbs1 <mbilalsadiq.19@gmail.com>
2023-09-10 10:47:06 +0100

    Temperature converter in console

---
+++ temperature.js
@@ -0,0 +1,12 @@
+/*
+* Converting Temperatures 
+*
+* Celsius-to-Fahrenheit formula 
+*
+* F = C x 1.8 + 32 
+*/
+
+let celsius = 10; 
+let fahrenheit = celsius * 1.8 + 32
+
+console.log(/* print out result*/); 
