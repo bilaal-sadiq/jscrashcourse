e80900d2ce5efa6d3f851801ce56c3b84ab6ad4d
mrmbs1 <mbilalsadiq.19@gmail.com>
2023-09-10 10:39:28 +0100

    variables javascript

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-console.log('Hello World'[10])
-console.log('12345'.length)
+let fullName = 'Bilaal Sadiq'
+
+let isRaining = true
+let temperature = 20
+let planet = 'Earth'
+
+temperature = temperature + 2
+
+console.log(temperature)
+
