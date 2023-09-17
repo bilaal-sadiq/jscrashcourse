699aee4d3abbc8876e7fdad4f1d373ac6309408c
mrmbs1 <mbilalsadiq.19@gmail.com>
2023-09-17 09:55:34 +0100

    calculator excrcise

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-let cash = 30
+let cash = 50
 let price = 40
+let difference = cash - price
 
 if (cash > price) {
-    console.log ("You paid extra - here's your change")
+    console.log(`You paid extra here ${difference} dollars change`)
 }
 else if (cash === price) {
     console.log("you paid the exact amount, have a nice day")
 }
 else {
-    console.log('not enough money - you still owe x dollars')
+    console.log(`not enough money - you still owe ${difference * -1} dollars`)
 }
