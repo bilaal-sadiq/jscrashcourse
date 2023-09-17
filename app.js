db0b7519ebbb56103114dd6920ef9a2f2d3e2d70
mrmbs1 <mbilalsadiq.19@gmail.com>
2023-09-17 10:00:15 +0100

    Comparison Operators

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 let cash = 50
 let price = 40
-let difference = cash - price
+let isStoreOpen = true
 
-if (cash > price) {
-    console.log(`You paid extra here ${difference} dollars change`)
-}
-else if (cash === price) {
-    console.log("you paid the exact amount, have a nice day")
-}
-else {
-    console.log(`not enough money - you still owe ${difference * -1} dollars`)
+if (cash >= price && isStoreOpen == true) {
+    console.log('print the receipt')
 }
