4cb72eec9790d77708dbe7ae83e632200f4584c8
mrmbs1 <mbilalsadiq.19@gmail.com>
2023-09-17 11:00:34 +0100

    Loops excercise

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-// DRY Dont repeat yourself
-let count = 1;
+// // DRY Dont repeat yourself
+// let count = 1;
 
-while(count <= 100) {
-    console.log(count)
-    count = count + 1;
-}
+// while(count <= 100) {
+//     console.log(count)
+//     count = count + 1;
+// }
 
-for (let i = 0; i < 100; i++) {
-    console.log(i)
-}
+// for (let i = 0; i < 100; i++) {
+//     console.log(i)
+// }
