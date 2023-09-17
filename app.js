92c4a9fa827fba427388a6d06deb993ddfb2fac1
mrmbs1 <mbilalsadiq.19@gmail.com>
2023-09-17 10:06:45 +0100

    False Values

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ let cash = 50
 let price = 40
 let isStoreOpen = true
 
-if (cash >= price && isStoreOpen == true) {
+if ("") {
     console.log('print the receipt')
+}
+
+else {
+    console.log("falsy value")
 }
