6fb2d154e72880f3017be98b2c2f081fca6ffe92
mrmbs1 <mbilalsadiq.19@gmail.com>
2023-12-04 19:27:23 +0000

    Effective way for using for loops with less lines

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 // DRY = Don't repeat yourself
-let count = 1;
+// let count = 1;
 
-while (count <= 100) {
-  console.log('This ran')
-    count = count + 1; 
+// while (count <= 100) {
+//  console.log('This ran')
+//    count = count + 1; 
+// }
+
+for (let i = 1; i <= 99 ; i++){
+  console.log(i + 1);
 }
